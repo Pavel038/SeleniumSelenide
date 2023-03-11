67c01a48fe8ed235e91ef0c0476f4085ab6c4f27
Pavel038 <98365181+Pavel038@users.noreply.github.com>
2023-03-11 23:02:53 +0700

    Update ChromeTest.java

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -39,8 +39,8 @@ class ChromeTest {
     void test() {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
-        driver.findElement(By.cssSelector("input[name='phone']")).sendKeys("+79996788965");
-        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79996788965");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("span[class='button__text']")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("p[data-test-id='order-success']")).getText().trim();
